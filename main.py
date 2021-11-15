949286a8200db76af7744fe8a0de1cbbcd1c1d77
thecodingolivetree <77493574+thecodingolivetree@users.noreply.github.com>
2021-11-15 17:39:20 +0100

    First working version with bootstrap and form submitting, basic routing

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, request, render_template
 
 app = Flask("Hello World")
 
-@app.route('/hello')
-def hello_world():
-    return render_template('index.html', name="Samira")
 
-@app.route('/passt')
-def test():
-    return "Passt!"
+@app.route("/", methods=['GET', 'POST'])  # "/" bedeutet es rootet immer zurück auf die Landingpage
+def hello():
+    return render_template("index.html", name="Du Schönheit!")
+
+
+@app.route("/create/subscription", methods=['GET'])
+def render_create_subscription_form():
+    return render_template("create_subscription.html", name="Du Schönheit!")
+
+
+@app.route("/save/subscription", methods=['POST'])
+def save_new_subscription():
+    print(request.form.get('sex1'))  # Bei Speichern des Formulars wird ein POST ausgegeben
+    print("new form submitted")
+    return render_template("index.html", name="Du Schönheit!")
+
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
     app.run(debug=True, port=5000)
