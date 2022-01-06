167ce1f3eaf43cd9c795a2dfebd38a6f48d8b15d
thecodingolivetree <77493574+thecodingolivetree@users.noreply.github.com>
2022-01-06 19:41:51 +0100

    admin page erstellt inklusive tabellen und kostenkalkulation

--- database_service.py
+++ database_service.py
@@ -1,44 +1,170 @@
-from flask import Flask, request, render_template
 import json
-# class Subscription:
-#     def __init__(self, beginnerdate, intermediatedate, advanceddate,
-#                  level, surfboard, insurance, transport, sex, name, vorname,
-#                  adresse, geburi, mail, tel, costs):
-#         self.beginnerdate = beginnerdate
-#         self.intermediatedate = intermediatedate
-#         self.advanceddate = advanceddate
-#         self.level = level
-#         self.surfboard = surfboard
-#         self.insurance = insurance
-#         self.transport = transport
-#         self.sex = sex
-#         self.name = name
-#         self.vorname = vorname
-#         self.adresse = adresse
-#         self.geburi = geburi
-#         self.mail = mail
-#         self.tel = tel
-#         self.costs = costs
-
 
 def save_subscription(beginnerdate, intermediatedate, advanceddate,
-                 level, surfboard, insurance, transport, sex, name, vorname,
-                 adresse, geburi, mail, tel, costs):
+                      level, surfboard, insurance, transport, sex, name, vorname,
+                      adresse, geburi, mail, tel, costs):
     get_costs_dict()
     d = open("data/subscriptions.json")
     datenspeicher_list = json.load(d)
 
-    datenspeicher_list.append({"level": level, "datumBeginner": beginnerdate,"datumIntermediate": intermediatedate, "datumAdvanced": advanceddate, "surfboard": surfboard, "versicherung": insurance, "geschlecht": sex,"name": name, "vorname": vorname,"adresse": adresse, "geburtstag": geburi, "mail": mail, "telefon": tel, "transport": transport, "costs": costs})
+    datenspeicher_list.append({"level": level, "datumBeginner": beginnerdate, "datumIntermediate": intermediatedate,
+                               "datumAdvanced": advanceddate, "surfboard": surfboard, "versicherung": insurance,
+                               "geschlecht": sex, "name": name, "vorname": vorname, "adresse": adresse,
+                               "geburtstag": geburi, "mail": mail, "telefon": tel, "transport": transport,
+                               "costs": costs})
 
     with open('data/subscriptions.json', 'w') as f:
-        json.dump(datenspeicher_list, f, indent=4, sort_keys=True, separators=(",", ":")) #https://docs.python.org/3/library/json.html
+        json.dump(datenspeicher_list, f, indent=4, sort_keys=True,
+                  separators=(",", ":"))  # https://docs.python.org/3/library/json.html
+
 
 def get_costs_dict():
     d = open("data/costs.json")
     costs = json.load(d)
     return costs
 
+
 def get_all_subscriptions():
     d = open("data/subscriptions.json")
     datenspeicher_list = json.load(d)
     return datenspeicher_list
+
+
+def calculate_optional_selection_cost(surfboard, transport, insurance):
+    cost_metrics = get_costs_dict()
+    costs = 0
+    if surfboard == "ja":
+        costs += cost_metrics['surfboard']
+    if transport == "ja":
+        costs += cost_metrics['transport']
+    if insurance == "ja":
+        costs += cost_metrics['insurance']
+    return costs
+
+
+def get_course_summary():
+    # Beginnercourses
+    course1 = {
+        "date": "10:00 Uhr, 22.02.2022",
+        "numberOfParticipants": 0,
+        "money": 0
+    }
+
+    course2 = {
+        "date": "14:00 Uhr, 22.02.2022",
+        "numberOfParticipants": 0,
+        "money": 0
+    }
+
+    course3 = {
+        "date": "08:00 Uhr, 23.02.2022",
+        "numberOfParticipants": 0,
+        "money": 0
+    }
+
+    # Intermediate courses
+    course4 = {
+        "date": "10:00 Uhr, 22.02.2022",
+        "numberOfParticipants": 0,
+        "money": 0
+    }
+    course5 = {
+        "date": "10:00 Uhr, 22.02.2022",
+        "numberOfParticipants": 0,
+        "money": 0
+    }
+    course6 = {
+        "date": "10:00 Uhr, 22.02.2022",
+        "numberOfParticipants": 0,
+        "money": 0
+    }
+    # Advanced courses
+    course7 = {
+        "date": "10:00 Uhr, 22.02.2022",
+        "numberOfParticipants": 0,
+        "money": 0
+    }
+    course8 = {
+        "date": "10:00 Uhr, 22.02.2022",
+        "numberOfParticipants": 0,
+        "money": 0
+    }
+    course9 = {
+        "date": "10:00 Uhr, 22.02.2022",
+        "numberOfParticipants": 0,
+        "money": 0
+    }
+
+    d = open("data/subscriptions.json")
+    datenspeicher_list = json.load(d)
+    print(datenspeicher_list)
+
+    d = open("data/costs.json")
+    costs = json.load(d)
+
+    for entry in datenspeicher_list:
+        print(entry)
+        if course1['date'] in entry['datumBeginner']:
+            course1['numberOfParticipants'] = course1['numberOfParticipants'] + 1
+            course1['money'] = course1['money'] + calculate_optional_selection_cost(entry['surfboard'],
+                                                                                    entry['transport'],
+                                                                                    entry['versicherung']) + 50
+        if course2['date'] in entry['datumBeginner']:
+            course2['numberOfParticipants'] = course2['numberOfParticipants'] + 1
+            course2['money'] = course2['money'] + calculate_optional_selection_cost(entry['surfboard'],
+                                                                                    entry['transport'],
+                                                                                    entry['versicherung']) + 50
+        if course3['date'] in entry['datumBeginner']:
+            course3['numberOfParticipants'] = course3['numberOfParticipants'] + 1
+            course3['money'] = course3['money'] + calculate_optional_selection_cost(entry['surfboard'],
+                                                                                    entry['transport'],
+                                                                                    entry['versicherung']) + 50
+
+        if course4['date'] in entry['datumIntermediate']:
+            course4['numberOfParticipants'] = course4['numberOfParticipants'] + 1
+            course4['money'] = course4['money'] + calculate_optional_selection_cost(entry['surfboard'],
+                                                                                    entry['transport'],
+                                                                                    entry['versicherung']) + 50
+
+        if course5['date'] in entry['datumIntermediate']:
+            course5['numberOfParticipants'] = course5['numberOfParticipants'] + 1
+            course5['money'] = course5['money'] + calculate_optional_selection_cost(entry['surfboard'],
+                                                                                    entry['transport'],
+                                                                                    entry['versicherung']) + 50
+
+        if course6['date'] in entry['datumIntermediate']:
+            course6['numberOfParticipants'] = course6['numberOfParticipants'] + 1
+            course6['money'] = course6['money'] + calculate_optional_selection_cost(entry['surfboard'],
+                                                                                    entry['transport'],
+                                                                                    entry['versicherung']) + 50
+
+        if course7['date'] in entry['datumAdvanced']:
+            course7['numberOfParticipants'] = course7['numberOfParticipants'] + 1
+            course7['money'] = course7['money'] + calculate_optional_selection_cost(entry['surfboard'],
+                                                                                    entry['transport'],
+                                                                                    entry['versicherung']) + 50
+
+        if course8['date'] in entry['datumAdvanced']:
+            course8['numberOfParticipants'] = course8['numberOfParticipants'] + 1
+            course8['money'] = course8['money'] + calculate_optional_selection_cost(entry['surfboard'],
+                                                                                    entry['transport'],
+                                                                                    entry['versicherung']) + 50
+
+        if course9['date'] in entry['datumAdvanced']:
+            course9['numberOfParticipants'] = course9['numberOfParticipants'] + 1
+            course9['money'] = course9['money'] + calculate_optional_selection_cost(entry['surfboard'],
+                                                                                    entry['transport'],
+                                                                                    entry['versicherung']) + 50
+
+    courses = []
+    courses.append(course1)
+    courses.append(course2)
+    courses.append(course3)
+    courses.append(course4)
+    courses.append(course5)
+    courses.append(course6)
+    courses.append(course7)
+    courses.append(course8)
+    courses.append(course9)
+    return courses
+    # "07:00 Uhr, 26.02.2022"
--- main.py
+++ main.py
@@ -1,32 +1,49 @@
 from flask import Flask, request, render_template
-import json
+
 import database_service
+
 app = Flask("Hello World")
 
-def calculate_costs(surfboard, transport,insurance, beginnerdate, intermediatedate, advanceddate): #Input Herr Odoni
-    return calculate_optional_selection_cost(surfboard, transport,insurance) + calculate_costs_lessons(beginnerdate, intermediatedate, advanceddate)
 
-def calculate_optional_selection_cost(surfboard, transport,insurance):
+def calculate_costs(surfboard, transport, insurance, beginnerdate, intermediatedate, advanceddate):  # Input Herr Odoni
+    return calculate_optional_selection_cost(surfboard, transport, insurance) + calculate_costs_lessons(beginnerdate,
+                                                                                                        intermediatedate,
+                                                                                                        advanceddate)
+
+
+def calculate_optional_selection_cost(surfboard, transport, insurance):
     cost_metrics = database_service.get_costs_dict()
-    costs=0
-    if surfboard=="ja":
+    costs = 0
+    if surfboard == "ja":
         costs += cost_metrics['surfboard']
-    if transport =="ja":
+    if transport == "ja":
         costs += cost_metrics['transport']
-    if insurance =="ja":
+    if insurance == "ja":
         costs += cost_metrics['insurance']
     return costs
+
 
 def calculate_costs_lessons(beginnerdate, intermediatedate, advanceddate):
     cost_metrics = database_service.get_costs_dict()
 
     total_course_count = len(beginnerdate) + len(intermediatedate) + len(advanceddate);
-    return total_course_count*cost_metrics['basic_lesson']
+    return total_course_count * cost_metrics['basic_lesson']
+
 
 @app.route("/admin/page", methods=['GET'])
 def ausgabe():
-    anmeldungen = database_service.get_all_subscriptions() #greift auf die Datenbank mit Anmeldung zu, dinger sind die anmeldungen.
-    return render_template("admin_page.html", dinger=anmeldungen)
+    kurse = database_service.get_course_summary()
+    anmeldungen = database_service.get_all_subscriptions()  # greift auf die Datenbank mit Anmeldung zu, dinger sind die anmeldungen.
+    total = get_total_of_all_courses(kurse)
+    return render_template("admin_page.html", dinger=anmeldungen, kurse=kurse, total=total)
+
+
+def get_total_of_all_courses(kurse):
+    total = 0;
+    for course in kurse:
+        total = total + course['money']
+    return total
+
 
 def auflisten():
     anmeldungen = ""
@@ -36,22 +53,24 @@ def auflisten():
 
     return anmeldungen_liste
 
+
 @app.route("/", methods=['GET', 'POST'])  # "/" bedeutet es rootet immer zurück auf die Landingpage
 def hello():
     return render_template("index.html", name="Du Schönheit")
+
 
 @app.route("/create/subscription", methods=['GET'])
 def render_create_subscription_form():
     return render_template("create_subscription.html", name="Du Schönheit")
 
+
 @app.route("/save/subscription", methods=['POST'])
 def save_new_subscription():
     if request.method == "POST":
-
         beginnerdate = request.form.getlist("beginnerdate")
         intermediatedate = request.form.getlist("intermediatedate")
         advanceddate = request.form.getlist("advanceddate")
-        level = request.form.get('level')  #Bei Speichern des Formulars wird ein POST ausgegeben
+        level = request.form.get('level')  # Bei Speichern des Formulars wird ein POST ausgegeben
         surfboard = request.form.get("surfboard")
         insurance = request.form.get("insurance")
         transport = request.form.get("transport")
@@ -62,26 +81,30 @@ def save_new_subscription():
         geburi = request.form.get("geburi")
         mail = request.form.get("mail")
         tel = request.form.get("tel")
-        costs=calculate_costs(surfboard, transport, insurance, beginnerdate, intermediatedate, advanceddate)
+        costs = calculate_costs(surfboard, transport, insurance, beginnerdate, intermediatedate, advanceddate)
 
         database_service.save_subscription(beginnerdate,
-                                                         request.form.getlist("intermediatedate"),
-                                                         request.form.getlist("advanceddate"),
-                                                         request.form.get('level'),
-                                                         request.form.get("surfboard"),
-                                                         request.form.get("insurance"),
-                                                         request.form.get("transport"),
-                                                         request.form.get("sex"),
-                                                         request.form.get("name"),
-                                                         request.form.get("vorname"),
-                                                         request.form.get("adresse"),
-                                                         request.form.get("geburi"),
-                                                         request.form.get("tel"),
-                                                         request.form.get("mail"),
-                                                         calculate_costs(surfboard, transport, insurance, beginnerdate, intermediatedate, advanceddate)
-                                                         )
-    return render_template("save_subscription.html", name=name, vorname=vorname, adresse=adresse, geburi=geburi, mail=mail, tel=tel, sex=sex,level=level, beginnerdate=beginnerdate, intermediatedate=intermediatedate, advanceddate=advanceddate,surfboard=surfboard, insurance=insurance, transport=transport, costs=costs)
+                                           request.form.getlist("intermediatedate"),
+                                           request.form.getlist("advanceddate"),
+                                           request.form.get('level'),
+                                           request.form.get("surfboard"),
+                                           request.form.get("insurance"),
+                                           request.form.get("transport"),
+                                           request.form.get("sex"),
+                                           request.form.get("name"),
+                                           request.form.get("vorname"),
+                                           request.form.get("adresse"),
+                                           request.form.get("geburi"),
+                                           request.form.get("tel"),
+                                           request.form.get("mail"),
+                                           calculate_costs(surfboard, transport, insurance, beginnerdate,
+                                                           intermediatedate, advanceddate)
+                                           )
+    return render_template("save_subscription.html", name=name, vorname=vorname, adresse=adresse, geburi=geburi,
+                           mail=mail, tel=tel, sex=sex, level=level, beginnerdate=beginnerdate,
+                           intermediatedate=intermediatedate, advanceddate=advanceddate, surfboard=surfboard,
+                           insurance=insurance, transport=transport, costs=costs)
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000) #IP Adresse ist das Gebäude, Port ist die Tür "wo es klingelt"
+    app.run(debug=True, port=5000)  # IP Adresse ist das Gebäude, Port ist die Tür "wo es klingelt"
